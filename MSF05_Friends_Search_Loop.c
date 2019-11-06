88c66f9478957d156d091e6417140aa625303869
Laurence Maar <56456030+laurencemaar@users.noreply.github.com>
2019-11-06 10:38:43 -0800

    Update MSF05_Friends_Search_Loop.c

--- MSF05_Friends_Search_Loop.c
+++ MSF05_Friends_Search_Loop.c
@@ -72,17 +72,7 @@ MSF05_Friends_Search_Loop( char *myFriendPages )
     		"Snapshot=t204.inf", 
     		"Mode=HTML", 
     		LAST);
-    /*
-    	web_url("index.cfm_3", 
-    		"URL=http://www.myspace.com/index.cfm?fuseaction=find&circuitaction=search&searchType=network&interesttype=&country=&searchBy=First&f_first_name=lauren&Submit=Find", 
-    		"TargetFrame=", 
-    		"Resource=0", 
-    		"RecContentType=text/html", 
-    		"Referer=http://search.myspace.com/index.cfm?fuseaction=find&Mytoken={tokenID}", 
-    		"Snapshot=t204.inf", 
-    		"Mode=HTML", 
-    		LAST);
-    */
+		
     	lr_message( lr_eval_string( "{friendID_count}" ) );
 
     	x = atoi( lr_eval_string( "{friendID_count}" ) );
@@ -134,63 +124,6 @@ MSF05_Friends_Search_Loop( char *myFriendPages )
 			noFriends++;
 		}
 
-    /*
-    	web_url("site=myspace&position=leaderboard&page=19001001&rand=523845803", 
-    		"URL=http://delb.mspaceads.com/html.ng/site=myspace&position=leaderboard&page=19001001&rand=523845803", 
-    		"TargetFrame=", 
-    		"Resource=0", 
-    		"RecContentType=text/html", 
-    		"Referer=http://www.myspace.com/index.cfm?fuseaction=find&circuitaction=search&searchType=network&interesttype=&country=&searchBy=First&f_first_name=lauren&Submit=Find", 
-    		"Snapshot=t205.inf", 
-    		"Mode=HTML", 
-    		LAST);
-    */
-
-    /*
-    	web_url("UsersOnline.html_3", 
-    		"URL=http://onlinenow.myspace.com/UsersOnline.html?UserIDList=683,3102,9284,10020,13014,13755,15726,16229,16329,18836", 
-    		"TargetFrame=", 
-    		"Resource=0", 
-    		"RecContentType=text/html", 
-    		"Referer=http://www.myspace.com/index.cfm?fuseaction=find&circuitaction=search&searchType=network&interesttype=&country=&searchBy=First&f_first_name=lauren&Submit=Find", 
-    		"Snapshot=t206.inf", 
-    		"Mode=HTML", 
-    		LAST);
-
-    */
-    /*
-    	web_add_cookie("adrevid=4223d091; DOMAIN=media.adrevolver.com");
-
-    	web_add_cookie("prefs=50-20-0-0-0+150-9-5-0-0+60-21-1-0-0+160-82-0-0-0; DOMAIN=media.adrevolver.com");
-
-    	web_add_cookie("freq=1121214725278+3-3+0-0+0-0+1-1+1-1+2-2+1-2+2-3+2-3+0-0+0-3+4-6+2-6+4-6+4-4+0-0+0-0+0-0+0-0+0-1+1-2+0-0+0-0+0-0+0-0+0-0+0-0+1-2+2-2+3-3+1-3; DOMAIN=media.adrevolver.com");
-
-    	web_add_cookie("uid=1799796134415585; DOMAIN=media.adrevolver.com");
-
-    	web_add_cookie("gai=131842; DOMAIN=media.adrevolver.com");
-
-    	web_add_cookie("prefs=50-20-0-0-0+150-16-5-0-0+60-22-1-0-0+160-82-0-0-0; DOMAIN=media.adrevolver.com");
-
-    	web_url("banner", 
-    		"URL=http://media.adrevolver.com/adrevolver/banner?place=1779&cpy=98556", 
-    		"TargetFrame=", 
-    		"Resource=0", 
-    		"RecContentType=text/html", 
-    		"Referer=http://delb.mspaceads.com/html.ng/site=myspace&position=leaderboard&page=19001001&rand=523845803", 
-    		"Snapshot=t207.inf", 
-    		"Mode=HTML", 
-    		LAST);
-
-    	web_url("NATIONAL.xml", 
-    		"URL=http://www.delta-offers.com/dr_banners/NATIONAL.xml?cacheKiller=1121214805986", 
-    		"TargetFrame=", 
-    		"Resource=0", 
-    		"RecContentType=text/xml", 
-    		"Referer=", 
-    		"Snapshot=t208.inf", 
-    		"Mode=HTML", 
-    		LAST);
-    */
     	lr_end_transaction("SearchLoop",LR_AUTO);
 
     	lr_think_time(3);
